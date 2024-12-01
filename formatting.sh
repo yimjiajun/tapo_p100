9093e148b434001e2379e3a5a0ad0a53fb76dfda
JiaJun Yim <yimjiajun@icloud.com>
2024-12-02 00:53:25 +0800

    feat: add script to format Python files with ruff
    This commit introduces a new script `formatting.sh` that formats Python
    files in the current directory using `ruff`. The script checks if `ruff`
    is installed and prompts the user to install it if not. It then finds
    all Python files in the current directory and formats them with a line
    length of 120 characters. If formatting fails, an error message is
    displayed and the script exits with a non-zero status.

---
+++ formatting.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ -z "$(which ruff)" ]; then
+  echo "ruff is not installed. Please install it usising 'pip install ruff'"
+fi
+
+files="$(find . -maxdepth 1 -type f -name "*.py"  -printf '%P\n')"
+
+if ! ruff format --line-length 120 $files; then
+  echo "Formatting failed. Please check the errors above."
+  exit 1
+fi
