d70b9c70cd9f804a5ae50f1307d76f37c1d92885
JiaJun Yim <yimjiajun@icloud.com>
2024-12-02 00:59:08 +0800

    fix formatting: exit when ruff is not installed

--- formatting.sh
+++ formatting.sh
@@ -2,6 +2,7 @@
 
 if [ -z "$(which ruff)" ]; then
   echo "ruff is not installed. Please install it usising 'pip install ruff'"
+  exit 1
 fi
 
 files="$(find . -maxdepth 1 -type f -name "*.py"  -printf '%P\n')"
